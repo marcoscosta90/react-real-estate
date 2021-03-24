87b241c72290a5163d35731709b043ee593b2218
marcoscosta90 <marcoscosta.dev@gmail.com>
2021-03-24 12:50:59 -0300

    Real estate in react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import GlobalStyle from './globalStyles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+        <Navbar />
+        <Hero />
+     
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import styled, { css } from 'styled-components/macro'
+import { Link } from 'react-router-dom'
+import { menuData } from '../data/MenuData';
+import { Button } from './Button';
+import { FaBars } from 'react-icons/fa'
+
+const Nav = styled.nav`
+    height: 60px;
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 2rem;
+    z-index: 100;
+    position: fixed;
+    width: 100%;
+    background: red;
+`;
+
+const NavLink = css`
+    color: #fff;
+    display: flex;
+    align-items: center;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+    text-decoration: none;
+`
+
+const Logo = styled(Link)`
+    ${NavLink}
+    font-style: italic;
+`;
+
+const MenuBars = styled(FaBars)`
+    display: none;
+
+    @media screen and (max-width: 768px) {
+        color: #fff;
+        display: block;
+        width: 35px;
+        height: 35px;
+        cursor: pointer;
+        position: absolute;
+        top: 0;
+        right: 0;
+        transform: translate(-50%, 35%);
+    }
+`;
+
+const NavMenu = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: -48px;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
+`;
+
+const NavMenuLinks = styled(Link)`
+    ${NavLink}
+    color: #fff;
+`;
+
+const NavBtn = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: 24px;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
+`;
+
+const Navbar = () => {
+    return (
+        <Nav>
+            <Logo to="/">Elixr</Logo>
+            <MenuBars />
+            <NavMenu>
+                {menuData.map((item, index) => (
+                    <NavMenuLinks to={item.link} key={index}>
+                        {item.title}
+                    </NavMenuLinks>
+                ))}
+            </NavMenu>
+            <NavBtn>
+                <Button to="/contact" primary="true">Contact us</Button>
+            </NavBtn>
+        </Nav>
+    )
+}
+
+export default Navbar
