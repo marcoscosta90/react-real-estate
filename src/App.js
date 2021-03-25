ae080135c2a3b8f6cbc278eb66e03a937eaed60d
marcoscosta90 <marcoscosta.dev@gmail.com>
2021-03-25 14:56:54 -0300

    Info Section implemented

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react'
 import './App.css';
 import Dropdown from './components/Dropdown';
 import Hero from './components/Hero';
+import InfoSection from './components/InfoSection';
 import Navbar from './components/Navbar';
+import { InfoData } from './data/InfoData';
 import { SliderData } from './data/SliderData';
 import GlobalStyle from './globalStyles';
 
@@ -13,13 +15,13 @@ function App() {
     setIsOpen(!isOpen)
   }
 
-
   return (
     <>
       <GlobalStyle />
         <Navbar toggle={toggle} />
         <Dropdown isOpen={isOpen} toggle={toggle} />
         <Hero slides={SliderData} />
+        <InfoSection {...InfoData}/>
      
     </>
   );
---
+++ src/data/InfoData.js
@@ -0,0 +1,23 @@
+import ImageOne from '../images/interior-1.jpg'
+import ImageTwo from '../images/house-5.jpg'
+
+
+export const InfoData = {
+    heading: 'Explore our beautiful homes',
+    paragraphOne: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit ea, quia consectetur eveniet sed aspernatur ab, quas voluptatem iusto, ipsam quod? Totam necessitatibus voluptates ipsa ipsum omnis exercitationem, eveniet delectus! ',
+    paragraphTwo: 'dicta voluptatibus unde.Vitae pariatur ipsa magni,libero dolorum voluptatem voluptates voluptatibus molestias deserunt enim veniam quam',
+    buttonLabel: 'View Homes',
+    image: ImageOne,
+    reverse: false,
+    delay: 100
+};
+
+export const InfoDataTwo = {
+    heading: 'Modern Designs',
+    paragraphOne: 'Lorem ipsum dolor elit. Fugit ea, quia consectetur eveniet sed aspernatur ab, quas voluptatem iusto, ipsam quod? Totam necessitatibus voluptates ipsa ipsum omnis exercitationem, eveniet delectus! ',
+    paragraphTwo: 'dicta voluptatibus und magni,libero dolorum voluptatem voluptates voluptatibus molestias deserunt enim veniam quam',
+    buttonLabel: 'View Homes',
+    image: ImageTwo,
+    reverse: true,
+    delay: 300
+};
